ccdc5f3cd3912edea03dcdacffba8a844f06a7c0
JoseALopezP <jalp42@live.com>
2022-07-19 02:11:29 -0300

    Improvements on functions selection system

--- index.js
+++ index.js
@@ -9,32 +9,42 @@ function Producto(codigo, marca, tipo, peso, precio){
     this.cambiarPrecio = (nuevoPrecio) => (this.precio = nuevoPrecio);
 }
 
-//declaracion del array de productos
-const productos=[];
+//Declaracion del array de productos
+let productos = [];
+//Seteo de productos
+obtenerProductosLocalStorage()
 
-//Agregando productos basicos
-let newProducto = new Producto(1, "La Paulina", "Queso Cremoso tradicional", 100, 86.3);
-productos.push(newProducto);
-newProducto = new Producto(2, "La Paulina", "Queso Reggianito", 100, 212.7);
-productos.push(newProducto);
-newProducto = new Producto(3, "La Paulina", "Queso Crema", 100, 78.1);
-productos.push(newProducto);
-newProducto = new Producto(4, "Paladini", "Mortadela", 100, 82.8);
-productos.push(newProducto);
-newProducto = new Producto(5, "Paladini", "Jamon Cocido", 100, 126.1);
-productos.push(newProducto);
-newProducto = new Producto(6, "Paladini", "Bondiola", 100, 213.2);
-productos.push(newProducto);
-newProducto = new Producto(7, "Paladini", "Salame Milan", 100, 156.7);
-productos.push(newProducto);
-newProducto = new Producto(8, "Paladini", "Queso Cremoso", 100, 97.5);
-productos.push(newProducto);
-newProducto = new Producto(9, "Paladini", "Queso Reggianito", 100, 191.9);
-productos.push(newProducto);
-newProducto = new Producto(10, "Paladini", "Queso Danbo", 100, 108.7);
-productos.push(newProducto);
-
-
+//Seteo inicial de productos (nada si ya hay productos, los basicos si aun no se ha agregado ninguno)
+function obtenerProductosLocalStorage() {
+    let productosAlmacenados = localStorage.getItem("productosLocal");
+    if (productosAlmacenados != '') {
+        console.log(typeof productosAlmacenados);
+        console.log(productosAlmacenados);
+        productos = JSON.parse(productosAlmacenados);
+    } else{
+        let newProducto = new Producto(1, "La Paulina", "Queso Cremoso tradicional", 100, 86.3);
+        productos.push(newProducto);
+        newProducto = new Producto(2, "La Paulina", "Queso Reggianito", 100, 212.7);
+        productos.push(newProducto);
+        newProducto = new Producto(3, "La Paulina", "Queso Crema", 100, 78.1);
+        productos.push(newProducto);
+        newProducto = new Producto(4, "Paladini", "Mortadela", 100, 82.8);
+        productos.push(newProducto);
+        newProducto = new Producto(5, "Paladini", "Jamon Cocido", 100, 126.1);
+        productos.push(newProducto);
+        newProducto = new Producto(6, "Paladini", "Bondiola", 100, 213.2);
+        productos.push(newProducto);
+        newProducto = new Producto(7, "Paladini", "Salame Milan", 100, 156.7);
+        productos.push(newProducto);
+        newProducto = new Producto(8, "Paladini", "Queso Cremoso", 100, 97.5);
+        productos.push(newProducto);
+        newProducto = new Producto(9, "Paladini", "Queso Reggianito", 100, 191.9);
+        productos.push(newProducto);
+        newProducto = new Producto(10, "Paladini", "Queso Danbo", 100, 108.7);
+        productos.push(newProducto);
+        localStorage.setItem("productosLocal", JSON.stringify(productos));
+    }
+}
 
 //funcion constructora para array con opciones de desarrollador
 function developerListOption(codigoD, descripcion){
@@ -112,32 +122,19 @@ function developerUser(){
             x = prompt("Seleccione el producto al que desea cambiar el precio");
             let p = prompt("Cual sera el nuevo precio?")
             productos[x-1].cambiarPrecio(parseFloat(p));
-            console.clear();
-            let productBlocks = document.querySelectorAll('.productBlock');
-            for(const product of productBlocks){
-                product.remove();
-            }
-            list(productos);
+            cleanListedProducts()
             listingProducts()
         }else if(id === "B"){
             productos.sort((a, b) => {
                 return b.precio - a.precio;
             });
-            let productBlocks = document.querySelectorAll('.productBlock');
-            for(const product of productBlocks){
-                product.remove();
-            }
-            list(productos);
+            cleanListedProducts()
             listingProducts()
         }else if(id === "C"){
             productos.sort((a, b) => {
                 return a.precio - b.precio;
             });
-            let productBlocks = document.querySelectorAll('.productBlock');
-            for(const product of productBlocks){
-                product.remove();
-            }
-            list(productos);
+            cleanListedProducts()
             listingProducts()
         }
         id = optionDeveloper();
@@ -174,19 +171,6 @@ function listingProducts(){
                 <h4>${product.tipo}</h4>
                 <p>Precio (${product.peso}gr): $${product.precio}</p>
             </div>
-            <div class="productBlockButtons">
-                <p>Cambiar Precio</p>
-                <form class="changePrice" id="changePrice${product.codigo}">
-                    <input type="number" step="0.01" placeholder="Ingresa el nuevo precio" id="priceChangePrice${product.codigo}">
-                    <input type="hidden" value="${product.codigo}">
-                    <button type="submit">CAMBIAR</button>
-                </form>
-                <p>Agregar al Carrito</p>
-                <form class="addCart" id="addCart${product.codigo}">
-                    <input type="number" step="0.01" placeholder="Ingresa la cantidad (gramos)" id="cantidadAddCart${product.codigo}">
-                    <button type="submit">AGREGAR</button>
-                </form>
-            </div>
         `
         if(product.marca == "La Paulina"){
             let productBlock = document.createElement("div");
@@ -208,8 +192,6 @@ function listingProducts(){
     }
     
 }
-
-listingProducts()
 
 //Formulario para listar nuevos productos
 let formularioProductos = document.getElementById("formularioProductos");
@@ -249,3 +231,66 @@ let btnCliente = document.getElementById("btnCliente");
 btnCliente.onclick = () => {
     carrito();
 };
+
+//Listado de opciones para funciones
+function listingOptions(){
+    /*
+    for(const product of productos){
+        option.innerHTML= `
+            <option value="${product.codigo}">${product.marca} ${product.tipo}</option>
+        `;
+        ${place}.append(option);
+    }
+    for(const product of productos){
+        option.innerHTML= `
+            <option value="${product.codigo}">${product.marca} ${product.tipo}</option>
+        `;
+        ${place}.append(option);
+    }
+    for(const product of productos){
+        option.innerHTML= `
+            <option value="${product.codigo}">${product.marca} ${product.tipo}</option>
+        `;
+        ${place}.append(option);
+    }*/
+    for(const product of productos){
+        let option = document.createElement("option");
+        option.value = `${product.codigo}`;
+        option.innerHTML= `
+            <option value="${product.codigo}">${product.marca} ${product.tipo}</option>
+        `;
+        deleteOptions.append(option);
+    }
+    for(const product of productos){
+        let option = document.createElement("option");
+        option.value = `${product.codigo}`;
+        option.innerHTML= `
+            <option value="${product.codigo}">${product.marca} ${product.tipo}</option>
+        `;
+        changePriceOptions.append(option);
+    }
+    for(const product of productos){
+        let option = document.createElement("option");
+        option.value = `${product.codigo}`;
+        option.innerHTML= `
+            <option value="${product.codigo}">${product.marca} ${product.tipo}</option>
+        `;
+        addCartOptions.append(option);
+    }
+    
+}
+
+/*let productBlock = document.createElement("div");
+            productBlock.className = "productBlock Paladini";
+            productBlock.innerHTML = htmlCode;
+            productList.append(productBlock);*/
+
+//Listado Individual
+
+function main(){
+    obtenerProductosLocalStorage();
+    listingProducts();
+    listingOptions();
+}
+
+main();
